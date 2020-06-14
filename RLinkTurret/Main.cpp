1cf01e8f1d025dd2998991a83748af175d6e7c5c
williamblood <w.blood@icloud.com>
2020-06-14 15:58:56 -0700

    created live video streaming via RTSP

--- RLinkTurret/Main.cpp
+++ RLinkTurret/Main.cpp
@@ -1,12 +1,15 @@
 #include <fstream>
+#include <assert.h>
 
 #include "opencv2/opencv.hpp"
 #include "FileHandler.h"
 
-
 using namespace std;
 
+const int fps = 25;
+
 string getCameraIPAddress();
+void renderStream(const string& rtspURL);
 
 int main()
 {
@@ -16,7 +19,30 @@ int main()
 		<< getCameraIPAddress()
 		<< "\n-------------------------------------\n" << endl;
 
+	string testURL = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+
+	renderStream(testURL);
+
 	return 0;
+}
+
+void renderStream(const string& rtspURL)
+{
+	cv::VideoCapture stream(rtspURL);
+	
+	assert(stream.isOpened());
+
+	cv::Mat frame;		// stores current frame 
+	cv::namedWindow("Livestream 1");
+
+	// 1000 miliseconds
+	while (stream.read(frame))
+	{
+		cv::imshow("Livestream 1", frame);
+
+		if (cv::waitKey(1000 / fps) >= 0)
+			return;
+	}
 }
 
 string getCameraIPAddress()
@@ -31,9 +57,7 @@ string getCameraIPAddress()
 		ipaFile.close();
 	}
 	else
-	{
 		cerr << "File not found.\n";
-	}
 
 	return ipAddress;
 }
