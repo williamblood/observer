4aaff6bd3482550fc5c15af9f413d23618c0a872
williamblood <w.blood@icloud.com>
2020-06-14 03:16:31 -0700

    ip address extracted from required .txt file

---
+++ RLinkTurret/FileHandler.h
@@ -0,0 +1,10 @@
+#ifndef FILE_HANDLER_
+#define FILE_HANDLER_
+
+#include <string>
+
+std::string const IP_ADDRESS_PATH( "C:\\Users\\wbloo\\OneDrive\\Desktop\\C\\OpenCV_IPC\\RLC_IP_ADDRESS.txt" );
+
+// TODO: Find RLC_IP_ADDRESS.txt file and extract address
+
+#endif // !FILE_HANDLER_!
--- RLinkTurret/Main.cpp
+++ RLinkTurret/Main.cpp
@@ -1,7 +1,39 @@
+#include <fstream>
+
 #include "opencv2/opencv.hpp"
+#include "FileHandler.h"
+
+
+using namespace std;
+
+string getCameraIPAddress();
 
 int main()
 {
 
+	cout << "\n-------------------------------------\n"
+		<< "[CAMERA]\nIP ADDRESS:\t" 
+		<< getCameraIPAddress()
+		<< "\n-------------------------------------\n" << endl;
+
 	return 0;
+}
+
+string getCameraIPAddress()
+{
+	ifstream ipaFile(IP_ADDRESS_PATH);
+	string ipAddress;
+
+	if (ipaFile.is_open())
+	{
+		getline(ipaFile, ipAddress);
+		
+		ipaFile.close();
+	}
+	else
+	{
+		cerr << "File not found.\n";
+	}
+
+	return ipAddress;
 }
